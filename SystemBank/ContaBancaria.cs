02c7b2357816129fda057936226331390fc616ef
Alex <freitas.alex.soares@outlook.com>
2021-09-07 18:30:17 -0300

    Add project files.

---
+++ SystemBank/ContaBancaria.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemBank
+{ 
+    class ContaBancaria
+    {   //atributos
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+
+
+        //construtores
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        //deposito
+        public void Deposito(Double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        //saque
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+            Saldo -= Saldo * 0.03; //custo do saque 0.03% do Saldo.
+        }
+
+        public override string ToString()
+        {
+            return "Conta"
+                + " "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+}
+
---
+++ SystemBank/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace SystemBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            //intro
+            Console.WriteLine("Bem vindo ao sistema interno SystemBank.");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+            Console.WriteLine("Abaixo informe dados do cliente.");
+            Console.WriteLine();
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            //resposta para pergunta
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Digite valor do deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial); //caso tenha saldo inicial.
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular); //caso não tenha saldo inicial, value = 0.
+            }
+
+            //exibe dados da conta.
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta); 
+
+            //deposito em conta.
+            Console.WriteLine();
+            Console.Write("Entre com um valor para deposito: ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valorDeposito);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            //saque em conta;
+            Console.WriteLine();
+            Console.WriteLine("Entre com um valor para saque: ");
+            Console.WriteLine("ATENÇÂO, para saque será cobrado valor de 0,03% do valor em conta.");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valorSaque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+        }
+    }
+}
